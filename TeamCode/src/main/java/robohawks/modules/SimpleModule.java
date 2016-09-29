c16827e19a13a6bead029e7668435c1cc2d24c84
Paarthri <paarthri@gmail.com>
2016-09-29 15:40:00 -0400

    Added Simplemodule.simple

--- TeamCode/src/main/java/robohawks/modules/SimpleModule.java
+++ TeamCode/src/main/java/robohawks/modules/SimpleModule.java
@@ -2,6 +2,9 @@ package robohawks.modules;
 
 import robohawks.async.Operation;
 import robohawks.async.Sequence;
+import robohawks.async.Sequencer;
+import robohawks.async.error.ErrorArgs;
+import robohawks.async.error.ErrorHandler;
 import robohawks.modules.base.DriveModule;
 
 /**
@@ -9,8 +12,10 @@ import robohawks.modules.base.DriveModule;
  */
 public class SimpleModule {
     private DriveModule driveModule;
+    private Sequencer sequencer;
 
-    public SimpleModule(DriveModule driveModule) {
+    public SimpleModule(Sequencer sequencer, DriveModule driveModule) {
+        this.sequencer = sequencer;
         this.driveModule = driveModule;
     }
 
@@ -18,19 +23,27 @@ public class SimpleModule {
         return new SimpleOperation(this);
     }
 
-    private class SimpleOperation implements Operation {
+    private class SimpleOperation implements Operation, ErrorHandler {
         private SimpleModule module;
+        private Sequence inflectionSequence;
+
         private SimpleOperation(SimpleModule module) {
             this.module = module;
         }
 
         @Override
         public void start(Sequence.Callback callback) {
-
+            inflectionSequence = module.sequencer.begin(module.driveModule.arc(1,.5, 1)).then(module.driveModule.arc(1, 1, .5));
+            inflectionSequence.setErrorHandler(this);
         }
 
         @Override
         public void loop(Sequence.Callback callback) {
+            if(inflectionSequence.isFinished()) callback.next();
+        }
+
+        @Override
+        public void handleError(Sequence sequence, ErrorArgs error) {
 
         }
     }
