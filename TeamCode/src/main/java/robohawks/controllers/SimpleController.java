28bfa533fb32d71f34e4eccbd91281567cc07421
Paarthri <paarthri@gmail.com>
2016-10-06 17:22:37 -0400

    Updated SimpleController

--- TeamCode/src/main/java/robohawks/controllers/SimpleController.java
+++ TeamCode/src/main/java/robohawks/controllers/SimpleController.java
@@ -1,6 +1,8 @@
 package robohawks.controllers;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import robohawks.async.Sequence;
 import robohawks.async.Sequencer;
 import robohawks.modules.SimpleModule;
 import robohawks.modules.base.DriveModule;
@@ -10,9 +12,18 @@ import robohawks.modules.base.DriveModule;
  */
 @Autonomous(name="Simple", group ="Sample")
 public class SimpleController extends Controller{
+    Sequence mainSequence;
+
     @Override
     public void init() {
         DriveModule driveModule = new DriveModule(hardwareMap);
-        sequencer.begin(driveModule.drive(2, 1, 1)).then(driveModule.drive(2, 1, .5));
+        mainSequence = sequencer.begin(driveModule.drive(2, 1, 1)).then(driveModule.drive(2, 1, .5));
+    }
+
+    @Override
+    public void loop() {
+        super.loop();
+
+        if(mainSequence.isFinished()) requestOpModeStop();k
     }
 }
