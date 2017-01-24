955ae71121797d3414c5038280ab3529e0f3fb5a
Paarthri <paarthri@gmail.com>
2017-01-24 08:05:57 -0500

    ActuatorController

--- TeamCode/src/main/java/robohawks/controllers/ActuatorController.java
+++ TeamCode/src/main/java/robohawks/controllers/ActuatorController.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import robohawks.async.Sequence;
 import robohawks.async.SimpleOperation;
 import robohawks.modules.base.ActuatorModule;
+import robohawks.modules.base.ColorModule;
 import robohawks.modules.base.WaitModule;
 
 /**
@@ -17,11 +18,18 @@ public class ActuatorController extends Controller{
     @Override
     public void init() {
         final ActuatorModule actuatorModule = new ActuatorModule(hardwareMap);
-        mainSequence = sequencer
-            .begin(actuatorModule.setActuator1Op(true))
-            .then(actuatorModule.setActuator1Op(false))
-            .then(actuatorModule.setActuator2Op(true))
-            .then(actuatorModule.setActuator2Op(false));
+        final ColorModule colorModule = new ColorModule(hardwareMap);
+        if (colorModule.isRednotBlue()){
+            mainSequence = sequencer
+                    .begin(actuatorModule.setActuator1Op(true))
+                    .then(new WaitModule(2000))
+                    .then(actuatorModule.setActuator1Op(false));
+        } else {
+            mainSequence = sequencer
+                    .begin(actuatorModule.setActuator2Op(true))
+                    .then(new WaitModule(2000))
+                    .then(actuatorModule.setActuator2Op(false));
+        }
     }
 
     @Override
