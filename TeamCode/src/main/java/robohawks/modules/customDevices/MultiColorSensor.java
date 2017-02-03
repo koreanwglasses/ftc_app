23cbd867823bbab1f9b70991917b43c12db761cb
Paarthri <paarthri@gmail.com>
2017-02-02 19:12:09 -0500

    MultiColorSensor

---
+++ TeamCode/src/main/java/robohawks/modules/customDevices/MultiColorSensor.java
@@ -0,0 +1,49 @@
+package robohawks.modules.customDevices;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+
+/**
+ * Created by paarth on 2/2/17.
+ */
+public class MultiColorSensor extends ColorSensor{
+    @Override
+    public int red() {
+        return 0;
+    }
+
+    @Override
+    public int green() {
+        return 0;
+    }
+
+    @Override
+    public int blue() {
+        return 0;
+    }
+
+    @Override
+    public int alpha() {
+        return 0;
+    }
+
+    @Override
+    public int argb() {
+        return 0;
+    }
+
+    @Override
+    public void enableLed(boolean enable) {
+
+    }
+
+    @Override
+    public void setI2cAddress(I2cAddr newAddress) {
+
+    }
+
+    @Override
+    public I2cAddr getI2cAddress() {
+        return null;
+    }
+}
